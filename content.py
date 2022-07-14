4bac72404643910e301e32085de998ec3c7e9c6c
KLkISKHORE <106374581+KLkISKHORE@users.noreply.github.com>
2022-07-14 14:18:58 +0530

    commit file update

--- content.py
+++ content.py
@@ -1,10 +1,12 @@
 from github import Github
 
 #generated access token
-access_token ="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"
+access_token ="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"
 
+all=[]
 login = Github(access_token)
 user = login.get_user()
 repo = user.get_repo("Testers")
-contents = repo.get_contents("Hare.py")
-print(contents)
+contents = repo.get_contents("Hari.txt")
+
+print(contents.sha)
